73e21ce3e20b82c6ebc88abc602e68e572a73c22
Honzi07 <nyisztor.zsolt@gmail.com>
2023-08-26 11:07:11 +0200

    Header modul with elements.

---
+++ src/assets/components/header.js
@@ -0,0 +1,20 @@
+import { content, createHTMLelement } from '../../index.js';
+
+export function createHeader() {
+  const header = document.createElement('header');
+  const nav = document.createElement('nav');
+  const h1 = createHTMLelement('h1', undefined, 'Gummy Bear');
+  const ul = document.createElement('ul');
+
+  const menuNames = ['Home', 'Menu', 'Contact'];
+
+  menuNames.forEach((name) => {
+    const li = createHTMLelement('li', undefined, name);
+    ul.appendChild(li);
+  });
+
+  content.appendChild(header);
+  header.appendChild(h1);
+  header.appendChild(nav);
+  nav.appendChild(ul);
+}
