9f37c00c4961c4180e143d902229dbfbcba8ec28
Honzi07 <nyisztor.zsolt@gmail.com>
2023-08-27 10:47:18 +0200

    Eventlistener for changing between the different pages.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './assets/css/reset.css';
 import './assets/css/style.css';
+import { createHeader } from './assets/components/header.js';
+import { createHomePage } from './assets/pages/home.js';
 import { createMenuPage } from './assets/pages/menu.js';
+import { createContactPage } from './assets/pages/contact.js';
 export { content, createHTMLelement };
 
 const content = document.querySelector('#content');
@@ -13,4 +16,20 @@ function createHTMLelement(tagName, className, content) {
   return element;
 }
 
-createMenuPage();
+createHeader();
+createHomePage();
+
+const navEl = document.querySelector('nav');
+
+navEl.addEventListener('click', function (e) {
+  if (e.target.innerText === 'Home') {
+    content.children[1].remove();
+    createHomePage();
+  } else if (e.target.innerText === 'Menu') {
+    content.children[1].remove();
+    createMenuPage();
+  } else if (e.target.innerText === 'Contact') {
+    content.children[1].remove();
+    createContactPage();
+  }
+});
