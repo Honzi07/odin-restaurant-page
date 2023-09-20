0d3f3ebdb0423d4174ae919615b39aef4e761c87
Honzi07 <nyisztor.zsolt@gmail.com>
2023-09-20 06:01:55 +0200

    Code cleanup.

--- src/index.js
+++ src/index.js
@@ -18,29 +18,9 @@ function createHTMLelement(tagName, className, content) {
 
 createHeader();
 createHomePage();
-// createMenuPage();
 
 const navEl = document.querySelector('nav');
 const menuBtn = document.querySelector('main');
-console.log(menuBtn);
-
-// menuBtn.addEventListener('click', function () {
-//   content.children[1].remove();
-//   createMenuPage();
-// });
-
-// navEl.addEventListener('click', function (e) {
-//   if (e.target.innerText === 'Home') {
-//     content.children[1].remove();
-//     createHomePage();
-//   } else if (e.target.innerText === 'Menu') {
-//     content.children[1].remove();
-//     createMenuPage();
-//   } else if (e.target.innerText === 'Contact') {
-//     content.children[1].remove();
-//     createContactPage();
-//   }
-// });
 
 [navEl, menuBtn].forEach((el) =>
   el.addEventListener('click', (e) => {
